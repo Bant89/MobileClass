8a3144e26ed947063c605be54e23db08803efb73
Bant89 <bant8009@gmail.com>
2019-08-13 08:14:18 -0400

    Added initial setup of the project

---
+++ Practicas/Practicas/Practicas/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Practicas.Models;
+
+namespace Practicas.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
