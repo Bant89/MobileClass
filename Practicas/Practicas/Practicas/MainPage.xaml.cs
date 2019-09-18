635fb51fbdfac441c27c55c8f4d9dd6419e156e2
Bant89 <bant8009@gmail.com>
2019-09-18 08:36:55 -0400

    Added the basic functionality

--- Practicas/Practicas/Practicas/MainPage.xaml.cs
+++ Practicas/Practicas/Practicas/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,5 +19,56 @@ namespace Practicas
         {
             InitializeComponent();
         }
+
+        private List<SKPath> paths = new List<SKPath>();
+        private Dictionary<long, SKPath> temporaryPaths = new Dictionary<long, SKPath>();
+
+        private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.AliceBlue);
+
+            var touchPathStroke = new SKPaint
+            {
+                Color = SKColors.OrangeRed,
+                StrokeWidth = 2,
+                Style = SKPaintStyle.Stroke
+            };
+
+            foreach (var touchPath in paths)
+            {
+                canvas.DrawPath(touchPath, touchPathStroke);
+            }
+        }
+
+        private void OnTouch(object sender, SKTouchEventArgs e)
+        {
+
+            switch (e.ActionType)
+            {
+                case SKTouchAction.Pressed:
+                    var p = new SKPath();
+                    p.MoveTo(e.Location);
+                    temporaryPaths[e.Id] = p;
+                    break;
+                case SKTouchAction.Moved:
+                    if (e.InContact)
+                        temporaryPaths[e.Id].LineTo(e.Location);
+                    break;
+                case SKTouchAction.Released:
+                    paths.Add(temporaryPaths[e.Id]);
+                    temporaryPaths.Remove(e.Id);
+                    break;
+            }
+
+            e.Handled = true;
+
+            // update the UI on the screen
+            ((SKCanvasView)sender).InvalidateSurface();
+
+        }
+
     }
 }
